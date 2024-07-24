3ec36c3fdc9000652d63a01f8ea131f71ddb24a1
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-07-25 06:38:57 +0700

    commit yang kedua

---
+++ app.js
@@ -0,0 +1,14 @@
+const { Api } = require("./models");
+
+const app = async () => {
+  try {
+    await Api.login();
+    await Api.getSetting();
+    const gate = await Api.getGate();
+    gate.scan();
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+app();
---
+++ models/index.js
@@ -0,0 +1,9 @@
+const Printer = require("./printer");
+const Api = require("./api");
+const Gate = require("./gate");
+
+module.exports = {
+  Api,
+  Gate,
+  Printer,
+};
