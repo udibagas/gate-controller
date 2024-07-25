09fde794af50e764895c62935b602e334b75c5d3
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-07-25 12:37:22 +0700

    test sound

---
+++ sounds/test.js
@@ -0,0 +1,5 @@
+const player = require("../models/player");
+
+player.play(
+  "./001 - Selamat datang silakan tekan tombol hijau untuk tiket parkir anda atau gunakan kartu identitas anda untuk masuk.mp3"
+);
