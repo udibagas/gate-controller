34da9f55362990f7878867f8dccc6638daaae3cf
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-08-03 14:08:44 +0700

    update

--- sounds/index.js
+++ sounds/index.js
@@ -1,6 +1,5 @@
 module.exports = {
-  SELAMAT_DATANG:
-    "007 - silakan tekan tombol hijau untuk tiket parkir anda atau tombol merah jika anda memerlukan bantuan petugas",
+  SELAMAT_DATANG: "Silakan Tekan Tombol.mp3",
   SILAKAN_AMBIL_TIKET: "002 - silakan ambil tiket terima kasih.mp3",
   TERIMAKASIH: "006 - terima kasih.mp3",
   MOHON_TUNGGU: "010 - Mohon Tunggu Security akan membukakan gate.mp3",
